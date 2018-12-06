2699cc2ed2843f209b09793c1d155ab7c1dce1f5
Cody Edgington <cody.edgington@gmail.com>
2018-12-06 09:50:14 -0800

    Finished, needs clean up

--- BlockPlacer.js
+++ BlockPlacer.js
@@ -1,9 +1,48 @@
-var out = "Output String\n";
+var resetValues = function(){
+    width = parseInt(widthSlider.value);
+    height = parseInt(heightSlider.value);
+    box = fillBox(width, height, box, '0');
+    answer.innerText = "";
+};
 
-var x = 15;
-var y = 10;
+
+var initializeInputs = function()
+{
+
+    var widthSlider = new Slider(document.getElementById('widthSlider'), {
+
+        formatter: function(value) {
+            return 'Current value: ' + value;
+        }
+    }).on('slide', resetValues);
+
+    var heightSlider = new Slider(document.getElementById('heightSlider'), {
+        formatter: function(value) {
+            return 'Current value: ' + value;
+        }
+    }).on('slide', resetValues);
+
+    var blockWidthSlider = new Slider(document.getElementById('blockWidthSlider'), {
+        formatter: function(value) {
+            return 'Current value: ' + value;
+        }
+    });
+
+    var blockHeightSlider = new Slider(document.getElementById('blockHeightSlider'), {
+        formatter: function(value) {
+            return 'Current value: ' + value;
+        }
+    });
+};
+
+initializeInputs();
+
+
+
+var width = 8;
+var height = 8;
 var box = [];
-var blocks = [[3,3], [1, 5], [6, 7]];
+var blocks = [[3, 3]];
 var nPlaced = 0;
 var moves = [];
 var startTime = performance.now();
@@ -43,22 +82,8 @@ var fillBox = function(x, y, box, token){
         }
         box[i] = ar;
     }
-    console.log(box);
     return box;
-}
-
-
-var printBox = function(box){
-    out += "-----\n";
-    for(var i = 0; i < box.length; i++){
-        for(var j = 0; j < box[0].length; j++){
-            out = out +  "[" + box[i][j] + "]";
-        }
-        out += "\n";
-    }
-    out += "-----\n";
-    print();
-}
+};
 
 
 var print = function(){
@@ -66,8 +91,6 @@ var print = function(){
 };
 
 var makeMove = function(x, y, blockId, box, token){
-    //
-    // try {
         var afterBox = placeBlock(x, y, blocks[blockId], box, token, false);
         moves.push([x, y, blockId, token]);
         return afterBox;
@@ -82,15 +105,7 @@ var undoMove = function(box){
         throw "No Moves To Undo";
     }
     var move = moves.pop();
-    // if(blocks[move[2]] + 1 >= blocks.length){
-    //
-    //     return undoMove();
-    // }
     box = placeBlock(move[0], move[1], blocks[move[2]], box, '0', true);
-    // if(Array.isArray(afterBox)){
-    //     moves.push(move);
-    //     return afterBox;
-    // }
     return [move[2] + 1, box];
 }
 
@@ -102,21 +117,7 @@ var revert = function(box){
         nextMove = result[0]
     }
     return result;
-}
-
-var incrementSpot = function(spot, box){
-    if(spot[0] + 1>= box[0].length){
-        spot[0] = 0;
-        spot[1] = spot[1] + 1;
-        if(spot[1] + 1>= box.length){
-            return null;
-        }
-        return spot
-    } else {
-        spot[0] += 1;
-        return spot;
-    }
-}
+};
 
 var getNextSpot = function(box, start){
     start = start || [0,0];
@@ -141,8 +142,13 @@ var isFull = function(box){
     return true;
 }
 
+var clearBlocks = function(){
+    blocks = [];
+    answer.innerText = "";
+}
 
-var getPossibleBoxes = function(box) {
+
+var getPossibleBoxes = function(box, getFirst) {
     var solutions = [];
     var spot = getNextSpot(box);
     var lim = 0;
@@ -150,15 +156,12 @@ var getPossibleBoxes = function(box) {
     var nextMove = 0;
     var looking = true;
     while (looking) {
-        //console.log(spot);
-        //printBox(box);
         try {
             backup = duplicate(box);
             box = makeMove(spot[0], spot[1], nextMove, box, '' + ++nPlaced);
             nextMove = 0;
         } catch(error){
             //out += error + "\n";
-            print();
             box = duplicate(backup);
             if(nextMove + 1 <= blocks.length){
                 nextMove++;
@@ -176,6 +179,7 @@ var getPossibleBoxes = function(box) {
         if(isFull(box)){
             console.log("solution");
             solutions.push(duplicate(box));
+            if(getFirst) return solutions;
         }
         spot = getNextSpot(box);
         if(!Array.isArray(spot)){
@@ -192,37 +196,200 @@ var getPossibleBoxes = function(box) {
 
     }
     return solutions;
-}
+};
+
+var isItPossible = function(){
+    box = fillBox(width, height, box, '0');
+    answer = document.getElementById("answer");
+    answer.innerText = "thinking...";
+    answer.innerText = getPossibleBoxes(box, true).length > 0 ? "Yes" : "No";
+    displaySolution(getPossibleBoxes(box, true));
+};
+
+var addBlock = function(){
+    if(blocks.length < 5){
+        answer.innerText = "";
+        blocks.push([parseInt(blockHeightSlider.value), parseInt(blockWidthSlider.value)]);
+        blocks.sort(function(a, b){
+            if((a[0] * a[1]) > (b[0] * b[1])){
+                return 1;
+            } else if ((a[0] * a[1]) < (b[0] * b[1])){
+                return -1;
+            }
+            return 0;
+        });
+    }
+};
 
 var printSolutions = function(solutions){
-    out += "\n Solutions\n"
-    /*for(var sol in solutions){
-        printBox(solutions[sol]);
-    }*/
     console.log(solutions);
     console.log(performance.now() - startTime)
-}
+};
 
 var duplicate = function(ar){
     var newArray = ar.map(function(arr) {
         return arr.slice();
     });
     return newArray;
+};
+
+var findAllSolutions = function(){
+    box = fillBox(width, height, box, '0');
+    downloadCanvas(displaySolution(getPossibleBoxes(box)));
+};
+/*    */
+var displaySolution = function(solutions){
+    var getD = function(){
+        return Math.floor(Math.random() * 5);
+    };
+
+    var getAr = function(size){
+        var ar = [];
+        for(var i = 0; i < size; i++){
+            ar.push(getD())
+        }
+        return ar;
+    }
+    var getDoubleAr = function(height, width){
+        var ar = [];
+        for(var i = 0; i < height; i++){
+            ar.push(getAr(width))
+        }
+        return ar;
+    }
+
+    var getSampleOut = function(amount, height, width){
+        var ar = [];
+        for(var i = 0; i < amount; i++){
+            ar.push(getDoubleAr(height, width))
+        }
+        return ar;
+    }
+
+    var colorCode = {};
+    var xIndex = 0;
+    var yIndex = 0;
+
+    var scale = 3;
+
+    var canvas = document.createElement("canvas");
+    var dimension = Math.ceil(Math.sqrt(solutions.length * (solutions[0].length + 1) * (solutions[0][0].length + 1))) + 10;
+    canvas.width = dimension * scale;
+    canvas.height = dimension  * scale;
+    const ctx = canvas.getContext('2d');
+
+    for(var n = 0; n < solutions.length; n++){
+        colorCode = {};
+        sampleAr = solutions[n];
+
+
+        for(var i = 0; i < sampleAr[0].length; i++){
+            for (var j = 0; j < sampleAr.length; j++){
+                if(!colorCode[sampleAr[j][i]]){
+                    colorCode[sampleAr[j][i]] = getRandomColor(Object.values(colorCode));
+                }
+                ctx.fillStyle = colorCode[sampleAr[j][i]];
+                ctx.fillRect((i + xIndex) * scale, (j + yIndex) * scale, 1 * scale, 1 * scale);
+
+            }
+        }
+        xIndex += sampleAr[0].length + 1;
+        if((xIndex + sampleAr[0].length)* scale > canvas.width){
+            yIndex+= sampleAr.length + 1;
+            xIndex = 0;
+
+        }
+    }
+
+    function getRandomColor(notFromList) {
+        notFromList = (notFromList || []);
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 3; i++) {
+            color += letters[Math.floor(Math.random() * 16)] + '0'; //add 0 to make colors "very different"
+        }
+        return notFromList.includes(color) ? getRandomColor(notFromList) : color;
+    }
+    return canvas;
+};
+
+var downloadCanvas = function(canvas){
+    var download = document.createElement('a');
+    download.href = canvas.toDataURL();
+    download.download = "solution";
+    download.click()
+}
+/*  */
+function setup() {
+    frameRate(10);
+    var a =document.getElementById("previewBox")
+    canvas = createCanvas(1000, 500);
+    canvas.id("previewCanvas");
+    canvas.parent("previewBox");
+    resetValues();
 }
 
-/* var newArray = currentArray.map(function(arr) {
-    return arr.slice();
-}); */
+function draw() {
+    background("#f0f0f5");
+    fill(255, 0, 0);
+    fill(200, 200, 200);
+    scale = 12;
 
-//Increment unit test
-/*var spot = [0, 0];
-while(spot != null){
-print();
-  out += box[spot[1]][spot[0]] + "\n";
-  spot = incrementSpot(spot, box);
-  console.log(spot);
-} */
+    /*The Text*/
+    textSize(20);
+    fill("#000000");
+    textAlign(RIGHT, TOP);
+    text("Can you fill this Box: ", 200, 100);
+    text("Preview: ", 600, 10);
+    text(" with these Blocks: ", 600, 100);
+    text( blocks.length + " / 5 blocks ", 600, 200);
+
+    /* The line */
+    strokeWeight(3);
+    line(0, 80, canvas.width, 80);
+
+    /*The Box */
+    var offsetX = 200;
+    var offsetY = 100;
+
+    for(var i = 0; i <= height; i++){
+        line(offsetX ,offsetY + ((0 + i) * scale), (width * scale) + offsetX,offsetY + ((0 + i) *  scale))
+    }
+
+    for(var j = 0; j <= width; j++){
+        line(offsetX + ((0 + j) * scale),  offsetY, offsetX + ((0 + j) * scale), offsetY + (height * scale))
+    }
+
+    /* The Preview */
+    offsetX = 650;
+    offsetY = 10;
+    var preHeight = parseInt(blockHeightSlider.value);
+    var preWidth = parseInt(blockWidthSlider.value);
+
+    for(var i = 0; i <= preWidth; i++){
+        line(offsetX ,offsetY + ((0 + i) * scale), (preHeight * scale) + offsetX,offsetY + ((0 + i) *  scale))
+    }
+    for(var i = 0; i <= preHeight; i++){
+        line(offsetX + ((i) * scale),  offsetY, offsetX + ((i) * scale), offsetY + (preWidth * scale))
+    }
+
+    /*The Blocks*/
+    offsetX = 650;
+    offsetY = 100;
+    for(var b in blocks){
+        var block = blocks[b];
+        for(var i = 0; i <= block[1]; i++){
+            line(offsetX ,offsetY + ((0 + i) * scale), (block[0] * scale) + offsetX,offsetY + ((0 + i) *  scale))
+        }
+        for(var i = 0; i <= block[0]; i++){
+            line(offsetX + ((i) * scale),  offsetY, offsetX + ((i) * scale), offsetY + (block[1] * scale))
+        }
+        offsetY += (block[1] + 1) * scale
+
+    }
 
 
-box = fillBox(x, y, box, '0');
-//printSolutions(getPossibleBoxes(box));
+}
+
+box = fillBox(width, height, box, '0');
+printSolutions(getPossibleBoxes(box));
---
+++ MazeGenerator.js
@@ -0,0 +1,234 @@
+
+var initializeInputs = function()
+{
+
+    var widthslider = new Slider(document.getElementById('widthSlider'), {
+
+        formatter: function(value) {
+            return 'Current value: ' + value;
+        }
+    }).on('slide', restart);
+
+    var heightSlider = new Slider(document.getElementById('heightSlider'), {
+        formatter: function(value) {
+            return 'Current value: ' + value;
+        }
+    }).on('slide', restart);
+
+    var speedSlider = new Slider(document.getElementById('speedSlider'), {
+        formatter: function(value) {
+            return 'Current value: ' + value;
+        }
+    }).on('slide', updateSpeed);
+};
+
+var updateSpeed = function(){
+    frameRate(parseInt(speedSlider.value));
+}
+
+var restart = function(thenSetup){
+    thenSetup = thenSetup || true;
+    cells = [];
+
+    motions = [];
+    width = (widthSlider.value * 20) + 40;
+    height = (heightSlider.value * 20) + 40;
+    for(var i = 0; i < (width / scale) - 2; i++){
+        var ar = [];
+        for(var j = 0; j < (height / scale) - 2; j++){
+            ar.push(new cell(i, j));
+        }
+        cells.push(ar);
+    }
+    walkerX = Math.floor(Math.random() * widthSlider.value);
+    walkerY = Math.floor(Math.random() * heightSlider.value);
+    canvas = createCanvas(width, height);
+    canvas.parent("mazeBox");
+    if(speedSlider.value == 0) redraw();
+    // /if(thenSetup){setup();}
+}
+
+
+    let width = 640;
+    let height = 900;
+    let scale = 20;
+    let cells = [];
+    let walkerX = 6;
+    let walkerY = 7;
+    let motions = [];
+    let canvas;
+
+
+
+    function setup() {
+        frameRate(5);
+        restart();
+        canvas = createCanvas(width, height);
+        canvas.parent("mazeBox");
+        // for(var i = 0; i < (width / scale) - 2; i++){
+        //     var ar = [];
+        //     for(var j = 0; j < (height / scale) - 2; j++){
+        //         ar.push(new cell(i, j));
+        //     }
+        //     cells.push(ar);
+        // }
+
+
+    }
+
+    function draw() {
+        background(0,0,255);
+        fill(255,0,0);
+        for(var i = 0; i < (width / scale) - 2; i++){
+            for(var j = 0; j < (height / scale) - 2; j++){
+                cells[i][j].draw();
+            }
+        }
+        var wxp = (walkerX * scale) + scale;
+        var wyp = (walkerY * scale) + scale;
+        fill(255, 20, 20)
+
+        walk();
+        if(motions.length == 0){
+            cells[walkerX][walkerY].deadend = true;
+
+        } else {
+            rect(wxp + 3, wyp + 3, scale - 6, scale -6);
+        }
+    }
+
+    function walk(){
+        var n = cells[walkerX][walkerY].unvisitedNeighbors();
+
+        var next = n[Math.floor(Math.random() * n.length)];
+        var cur = cells[walkerX][walkerY];
+        if(!next){ //Stuck, need to back up
+            var dir = motions.pop(); //Top, Right, Bottom, Left
+            dir = (dir + 2) % 4
+            switch(dir){
+                case 0:
+                    cur.visit(0);
+                    walkerY--;
+                    break;
+                case 1:
+                    cur.visit(1);
+                    walkerX++;
+                    break;
+                case 2:
+                    cur.visit(2);
+                    walkerY++;
+                    break;
+                case 3:
+                    cur.visit(3);
+                    walkerX--;
+                    break;
+            }
+            return;
+        }
+        if(cur.x < next.x){ // Moving Right
+            next.visit(3);
+            cur.visit(1);
+            walkerX++;
+            motions.push(1);
+        } else if (cur.x > next.x){ //Moving Left
+            next.visit(1);
+            cur.visit(3);
+            walkerX--;
+            motions.push(3);
+        } else if (cur.y > next.y){ //moving up
+            next.visit(2);
+            cur.visit(0);
+            walkerY--;
+            motions.push(0);
+        } else if (cur.y < next.y){ //moving down
+            next.visit(0);
+            cur.visit(2);
+            walkerY++;
+            motions.push(2);
+        }
+    }
+
+    function exp(){
+        console.log("Exp");
+        var out = {};
+        for(var i = 0; i < cells.length; i++){
+            var row = {}
+            for(var j = 0; j < cells[0].length; j++){
+                var cell = {}
+                cell["x"] = cells[i][j].x;
+                cell["y"] = cells[i][j].y;
+                row[j] = cell;
+            }
+            out[i] = row
+        }
+
+        console.log(JSON.stringify(out));
+    }
+
+    function cell(x, y){
+        this.x = x;
+        this.y = y;
+        this.xp = (x * scale) + scale;
+        this.yp = (y * scale) + scale;
+        this.walls = [true, true, true, true]; //Top, Right, Bottom, Left
+        this.visited = Math.random() > 1.1;
+        this.deadend = false;
+
+        this.unvisitedNeighbors = function(){
+            var n = this.getNeighbors();
+            var un = [];
+            for(var c in n){
+                if(!n[c].visited)
+                    un.push(n[c]);
+            }
+            return un;
+        }
+
+        this.visit = function(side){ //02 and 13
+            this.visited = true;
+            this.walls[side] = false;
+            var ns = this.unvisitedNeighbors();
+            this.deadend = ns.length === 0;
+
+
+        }
+
+        this.getNeighbors = function(){
+            var neighbors = []
+            if(y + 1 < cells[0].length && cells[x][y+1])
+                neighbors.push(cells[x][y+1]);//Up
+            if(y > 0 && cells[x][y-1])
+                neighbors.push(cells[x][y-1])//Down
+            if(x + 1 < cells.length && cells[x+1][y])
+                neighbors.push(cells[x+1][y])//Right
+            if(x > 0 && cells[x-1][y])
+                neighbors.push(cells[x-1][y])//Left
+            return neighbors;
+        }
+
+
+        this.draw = function(){
+            if(this.deadend){
+                fill(200, 200, 200);
+            } else if(this.visited)
+                fill(100, 255, 100);
+            else
+                fill(100, 100, 255);
+            strokeWeight(0);
+            rect(this.xp, this.yp, scale, scale);
+            strokeWeight(5);
+            fill(255,0,0);
+            var w = 1; //Wall size. Larger W => smaller walls
+            if(this.walls[0])
+                line(this.xp + w, this.yp+w, this.xp + scale - w, this.yp +w);
+            if(this.walls[3])
+                line(this.xp + w, this.yp + w, this.xp +w, this.yp + scale -w);
+            if(this.walls[1])
+                line(this.xp + scale - w, this.yp +w, this.xp + scale -w, this.yp + scale-w);
+            if(this.walls[2])
+                line(this.xp +w, this.yp + scale-w, this.xp + scale-w, this.yp + scale-w);
+
+        }
+    }
+
+initializeInputs();
